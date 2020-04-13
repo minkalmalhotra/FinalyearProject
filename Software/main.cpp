536c6ddf661914b6c5a669b01ae0172d7e097f91
navneet kaur <40808379+Navneet95@users.noreply.github.com>
2020-04-12 20:07:49 -0400

    Create main.cpp

---
+++ Software/main.cpp
@@ -0,0 +1,11 @@
+#include "smartmirrorfinal.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    SmartMirrorFinal w;
+    w.show();
+
+    return a.exec();
+}
